123d9438e8f8dff445d2723fca1722a3afd947a3
zyddnys <zyddnys@outlook.com>
2019-11-19 23:45:23 -0500

    latest

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 g++ -fconcepts -Ofast -s -static -std=c++2a -o autocomplete.app autocomplete.cpp
-docker build --no-cache -t patchyvideo-autocomplete:$(date +"%Y-%m-%d") .
+docker build --no-cache -t patchyvideo-autocomplete:latest .
