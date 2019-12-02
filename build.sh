a405069b1a5d9c84d24d8e5ecd62175089628eab
zyddnys <zyddnys@outlook.com>
2019-12-02 16:58:16 -0500

    g++ upgrade

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-g++ -fconcepts -Ofast -s -static -std=c++2a -o autocomplete.app autocomplete.cpp
+g++ -Ofast -s -static -std=c++2a -o autocomplete.app autocomplete.cpp
 docker build --no-cache -t patchyvideo-autocomplete:latest .
